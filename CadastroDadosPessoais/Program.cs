aee613c5a0936e4b183c4ac5826f8f767afb03f0
karen <karen@LAPTOP-29ULN2KH>
2024-02-25 11:44:56 -0300

    Add project files.

---
+++ CadastroDadosPessoais/Program.cs
@@ -0,0 +1,15 @@
+﻿//Solicite ao usuário que digite seu nome
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine();
+
+//Solicite que o usuário digite seu sobrenome
+Console.WriteLine("Digite seu sobrenome: ");
+string sobrenome = Console.ReadLine();
+
+//Solicite ao usuário que digite sua idade
+Console.WriteLine("Digite sua idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+//Imprime o nome completo e idade do usuário 
+Console.WriteLine("Nome completo: " + nome + " " + sobrenome + " e sua idade é: " + idade + " anos ");
+
